196b5c2afbf292d3c64840930c763d02a27cf0ff
omarfaruktaj <omarfaruk3393@gmail.com>
2024-06-03 11:17:03 +0600

    task model added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,19 +1,22 @@
 const { default: mongoose } = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  Name: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    Name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    profile: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  profile: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
